beb256450bfc01b808134307fcfba6be830c6ebf
Ryan Fitzgerald <ryan@codebrewstudios.com>
2013-04-16 18:00:34 -0400

    foundation functions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-module.exports = require('./lib');
+var util = require("util"),
+    CircuitBreaker = require('./lib');
+
+module.exports = function(func, options) {
+  var breaker = new CircuitBreaker(func, options);
+
+  var result = function () {
+    return breaker.invoke.apply(breaker, arguments);
+  };
+
+  // public api methods to expose
+  var methods = [
+    'isOpen',
+  ];
+
+  methods.forEach(function (method) {
+    result[method] = function () {
+      return breaker[method].apply(breaker, arguments);
+    };
+  });
+
+  return result;
+};
--- test/circuitbreaker-test.js
+++ test/circuitbreaker-test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var CircuitBreaker = require('../index');
+var circuitBreaker = require('../index');
 
 require('chai').should();
 
@@ -13,18 +13,22 @@ describe('Circuit Breaker', function(){
       return callback(null, 'hello ' + name);
     };
 
-    breaker = CircuitBreaker(func, {timeout: 10, resetTimeout: 10});
+    breaker = circuitBreaker(func, {timeout: 10, resetTimeout: 10});
   });
 
   describe('stuff', function() {
 
     it('should work', function(done){
       breaker('bob').then(function (msg) {
-        console.log("message is", msg);
+        console.log('message is', msg);
       }).fail(function(err) {
-        console.log("error is", err);
+        console.log('error is', err);
       }).done(function() {return done();});
 
+    });
+
+    it('should be open', function () {
+      breaker.isOpen().should.be.true;
     });
   });
 });
