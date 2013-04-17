7b65cc9d1f4e961d06f58653475d9b855682f51d
Ryan Fitzgerald <ryan@codebrewstudios.com>
2013-04-16 20:15:57 -0400

    first stab at impl

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = function(func, options) {
   // public api methods to expose
   var methods = [
     'isOpen',
+    'isClosed'
   ];
 
   methods.forEach(function (method) {
--- test/circuitbreaker-test.js
+++ test/circuitbreaker-test.js
@@ -1,34 +1,60 @@
 'use strict';
 
-var circuitBreaker = require('../index');
+var CircuitBreaker = require('../lib/index'),
+    sinon          = require('sinon');
 
 require('chai').should();
 
 describe('Circuit Breaker', function(){
-  var breaker;
+  var breaker,
+      callback;
 
   beforeEach(function () {
-
+    callback = sinon.stub();
     var func = function (name, callback) {
-      return callback(null, 'hello ' + name);
+      return callback(new Error('foo'));
+      //return callback(null, 'hello ' + name);
     };
 
-    breaker = circuitBreaker(func, {timeout: 10, resetTimeout: 10});
+    breaker = new CircuitBreaker(func, {timeout: 10, resetTimeout: 10, maxFailures: 3});
   });
 
   describe('stuff', function() {
 
     it('should work', function(done){
-      breaker('bob').then(function (msg) {
+      breaker.invoke('bob').then(function (msg) {
         console.log('message is', msg);
       }).fail(function(err) {
         console.log('error is', err);
       }).done(function() {return done();});
 
     });
+  });
 
-    it('should be open', function () {
-      breaker.isOpen().should.be.true;
+  describe('#constructor', function () {
+    it('should be in closed state', function () {
+      var breaker = new CircuitBreaker(callback);
+      breaker.isClosed().should.be.true;
+    });
+
+    it('should zero out current number of failures', function () {
+      var breaker = new CircuitBreaker(callback);
+      breaker._numFailures.should.equal(0);
     });
   });
+
+  describe('#forceOpen', function () {
+
+    it('should enter open state', function () {
+      var breaker = new CircuitBreaker(callback);
+      breaker.isClosed().should.be.true;
+
+      breaker.forceOpen();
+
+      breaker.isOpen().should.be.true;
+      breaker.isClosed().should.be.false;
+    });
+
+  });
+
 });
