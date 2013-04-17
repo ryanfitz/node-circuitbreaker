b784d2342fa260ba4021d8751068aafd46407e2f
Ryan Fitzgerald <ryan@codebrewstudios.com>
2013-04-16 20:29:37 -0400

    testing out state transitions

--- test/circuitbreaker-test.js
+++ test/circuitbreaker-test.js
@@ -57,4 +57,59 @@ describe('Circuit Breaker', function(){
 
   });
 
+  describe('#forceClosed', function () {
+
+    it('should enter closed state', function () {
+      var breaker = new CircuitBreaker(callback);
+
+      breaker.forceOpen();
+      breaker.isOpen().should.be.true;
+
+      breaker.forceClosed();
+
+      breaker.isClosed().should.be.true;
+      breaker.isOpen().should.be.false;
+    });
+
+    it('should reset number of failrues to 0', function () {
+      var breaker = new CircuitBreaker(callback);
+      breaker._numFailures = 10;
+
+      breaker.forceClosed();
+
+      breaker._numFailures.should.equal(0);
+    });
+
+  });
+
+  describe('#handleFailure', function () {
+    it('should increment number of failures', function () {
+      var breaker = new CircuitBreaker(callback);
+      breaker.handleFailure();
+
+      breaker._numFailures.should.equal(1);
+    });
+
+    it('should open circuit after max failures is reached', function () {
+      var breaker = new CircuitBreaker(callback, {maxFailures: 3});
+
+      breaker.handleFailure();
+      breaker.handleFailure();
+      breaker.handleFailure();
+
+      breaker.isClosed().should.be.false;
+      breaker.isOpen().should.be.true;
+    });
+
+    it('should open circuit when in half-open state', function () {
+      var breaker = new CircuitBreaker(callback);
+      breaker.forceHalfOpen();
+
+      breaker.handleFailure();
+
+      breaker.isOpen().should.be.true;
+      breaker.isClosed().should.be.false;
+    });
+  });
+
 });
